4385f51d87aba3058499309d206b82a7e9620866
bookpark <bkbkgg@gmail.com>
2022-11-24 13:39:14 +0900

    글 상세 컴포넌트 function 형식으로 변경

--- src/components/DetailBoard.js
+++ src/components/DetailBoard.js
@@ -1,76 +1,75 @@
 import axios from "axios";
-import { Component } from "react";
-import './WriteBoard.css';
+import { useEffect, useState } from "react"
+import { useParams } from 'react-router-dom'
 
-class DetailBoard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            writer: '',
-            subject: '',
-            content: '',
-            imageUrl: ''
-        }
-    }
 
-    componentDidMount = () => {
-        axios.get('http://localhost:8080/api/board-detail/2')
+function DetailBoard() {
+
+    const [writer, setWriter] = useState('');
+    const [subject, setSubject] = useState('');
+    const [content, setContent] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/board-detail/${id}`)
             .then((response) => {
                 const board = response.data;
-                this.setState({ writer: board.writer, subject: board.subject, content: board.content, imageUrl: 'http://localhost:8080/img/' + board.filename });
+                setWriter(board.writer);
+                setSubject(board.subject);
+                setContent(board.content);
+                setImageUrl('http://localhost:8080/img/' + board.filename);
             })
             .catch((error) => {
-
+                console.log(error)
             })
-    }
+    })
 
-    render() {
-        return (
-            <section>
-                <h2>게시판 글 상세</h2>
-                <form>
-                    <table>
-                        <tr>
-                            <td className='td_left'>
-                                <label htmlFor='writer'>글쓴이</label>
-                            </td>
-                            <td className='td_right'>
-                                <input type='text' name='writer' value={this.state.writer} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className='td_left'>
-                                <label htmlFor='subject'>제목</label>
-                            </td>
-                            <td className='td_right'>
-                                <input type='text' name='subject' value={this.state.subject} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className='td_left'>
-                                <label htmlFor='content'>내용</label>
-                            </td>
-                            <td className='td_right'>
-                                <textarea type='text' name='content' cols='40' rows='15' value={this.state.content} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className='td_left'>
-                                <label htmlFor='image'>이미지</label>
-                            </td>
-                            <td className='td_right'>
-                                <img src={this.state.imageUrl} alt='' width={'200px'} height={'200px'} />
-                            </td>
-                        </tr>
-                    </table>
-                    <section id='commandCell'>
-                        <a href="#">수정</a>&nbsp;&nbsp;
-                        <a href="#">삭제</a>
-                    </section>
-                </form>
-            </section>
-        )
-    }
+    return (
+        <section>
+            <h2>게시판 글 상세</h2>
+            <form>
+                <table>
+                    <tr>
+                        <td className='td_left'>
+                            <label htmlFor='writer'>글쓴이</label>
+                        </td>
+                        <td className='td_right'>
+                            <input type='text' name='writer' value={writer} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='td_left'>
+                            <label htmlFor='subject'>제목</label>
+                        </td>
+                        <td className='td_right'>
+                            <input type='text' name='subject' value={subject} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='td_left'>
+                            <label htmlFor='content'>내용</label>
+                        </td>
+                        <td className='td_right'>
+                            <textarea type='text' name='content' cols='40' rows='15' value={content} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='td_left'>
+                            <label htmlFor='image'>이미지</label>
+                        </td>
+                        <td className='td_right'>
+                            <img src={imageUrl} alt='' width={'200px'} height={'200px'} />
+                        </td>
+                    </tr>
+                </table>
+                <section id='commandCell'>
+                    <a href="#">수정</a>&nbsp;&nbsp;
+                    <a href="#">삭제</a>
+                </section>
+            </form>
+        </section>
+    )
 }
 
 export default DetailBoard;
